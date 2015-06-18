485de7790db02e7dea4e86866ea77ab1b05cf215
ertrue <zhanghongbo1209@gmail.com>
2015-06-18 11:26:53 -0700

    Add the third and fourth page of the view controller.

---
+++ Sprite_IOS_V0/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Sprite_IOS_V0
+//
+//  Created by Hongbo Zhang on 6/18/15.
+//  Copyright (c) 2015 Hongbo Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
